461f8e6c46d63323d3bc76a103e4304a3729289b
Rustam Cafarov <RustamCafarov@embawood.dm>
2023-02-23 14:49:25 +0400

    initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='card'>Card</div>
+  )
+};
+
+const cards = [
+    { title: 'Card 1', description: 'This is the first card.' },
+    { title: 'Card 2', description: 'This is the second card.' },
+    { title: 'Card 3', description: 'This is the third card.' }
+  ];
+  
+
+
+export default Card;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header () {
+    return (
+        <ul className="header">
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+        )
+}
+
+
+export default Header
